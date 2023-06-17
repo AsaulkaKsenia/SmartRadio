3e7d1cbbfe78f8aaa9cefb864fac14a7ea439acd
Asaulka Ksenia <asa.ru.80@mail.ru>
2023-06-17 23:37:24 +0300

    Lombok

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,5 +1,13 @@
 package ru.netology.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
 public class Radio {
     private int minStation = 0;
     private int maxStation = 10;
@@ -8,94 +16,4 @@ public class Radio {
     private int minVolume = 0;
     private int currentVolume;
 
-
-    public int getCurrentStation() {
-
-        return currentStation;
-    }
-
-    public int getMaxStation() {
-
-        return maxStation;
-    }
-
-    public int getMinStation() {
-
-        return minStation;
-    }
-
-    public Radio() {
-
-        this.maxStation = getMaxStation();
-        this.minStation = getMinStation();
-        currentStation = getCurrentStation();
-    }
-
-    public Radio(int maxStation) {
-        this.maxStation = maxStation;
-        minStation = getMinStation();
-        currentStation = getCurrentStation();
-    }
-
-    public void setCurrentStation(int currentStation) {
-        if (currentStation > getMaxStation()) {
-
-            return;
-        }
-        if (currentStation < getMinStation()) {
-
-            return;
-        }
-
-        this.currentStation = currentStation;
-    }
-
-    public void nextStation() {
-        if (getCurrentStation() == getMaxStation()) {
-            setCurrentStation(minStation);
-        } else {
-            setCurrentStation(getCurrentStation() + 1);
-        }
-    }
-
-    public void prevStation() {
-        if (getCurrentStation() == minStation) {
-            setCurrentStation(getMaxStation());
-        } else {
-            setCurrentStation(getCurrentStation() - 1);
-        }
-    }
-
-
-    public int getCurrentVolume() {
-
-        return currentVolume;
-    }
-
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume > maxVolume) {
-            return;
-        }
-        if (newCurrentVolume < minVolume) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
-    }
-
-    public void louderVolume() {
-        if (getCurrentVolume() == maxVolume) {
-            return;
-        } else {
-            setCurrentVolume(getCurrentVolume() + 1);
-        }
-    }
-
-    public void silenceVolume() {
-        if (getCurrentVolume() == minVolume) {
-            return;
-        } else {
-            setCurrentVolume(getCurrentVolume() - 1);
-        }
-    }
 }
